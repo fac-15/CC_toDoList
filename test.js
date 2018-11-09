27dfd283322eec7eeae870b03db924bb13082208
Lucy <lewright20@gmail.com>
2018-11-09 11:14:41 +0000

    adding to do fixed

--- test.js
+++ test.js
@@ -1,58 +1,60 @@
-var test = require('tape');
-var tapSpec = require('tap-spec');
-var logic = require('./logic');
+var test = require("tape");
+var tapSpec = require("tap-spec");
+var logic = require("./logic");
 
 test("tape is working", function(t) {
   t.deepEquals(1, 1, "1 should equal 1");
   t.end();
 });
 
-test('clone array of objects', function(t) {
+test("clone array of objects", function(t) {
   var actual = logic.cloneArrayOfObjects([]);
-  var expected = []
+  var expected = [];
   t.deepEquals(actual, expected, "should clone array of objects");
   t.end();
 });
 
-
-test('adding a todo', function(t) {
-  var actual = logic.addTodo([], {a: 1});
-  var expected = [ { description: { a: 1 }, done: false, id: 1 } ]
+test("adding a todo", function(t) {
+  var actual = logic.addTodo([], { description: abc, done: false });
+  var expected = [{ description: abc, done: false, id: 1 }];
   t.deepEquals(actual, expected, "should add object to array");
   t.end();
 });
 
-test('adding a todo', function(t) {
-  var actual = logic.addTodo([{a: 1, id: -1}], {b: 2});
-  var expected = [{a: 1, id: -1}, { description: { b: 2 }, done: false, id: 2 } ]
+test("adding a todo", function(t) {
+  var actual = logic.addTodo([{ a: 1, id: -1 }], { b: 2 });
+  var expected = [
+    { a: 1, id: -1 },
+    { description: { b: 2 }, done: false, id: 2 }
+  ];
   t.deepEquals(actual, expected, "should add object to array");
   t.end();
 });
 
-test('test id', function(t) {
-  var actual = logic.addTodo([{id: -1}], {a: 1});
-  var expected = [{id: -1}, { description: { a: 1 }, done: false, id: 3 }]
+test("test id", function(t) {
+  var actual = logic.addTodo([{ id: -1 }], { a: 1 });
+  var expected = [{ id: -1 }, { description: { a: 1 }, done: false, id: 3 }];
   t.deepEquals(actual, expected, "should add id to new object");
   t.end();
 });
 
-test('adding a todo', function(t) {
+test("adding a todo", function(t) {
   var actual = logic.addTodo([], {});
-  var expected = [{ description: {}, done: false, id: 4 }]
+  var expected = [{ description: {}, done: false, id: 4 }];
   t.deepEquals(actual, expected, "should add object to array");
   t.end();
 });
 
-test('filter to delete', function(t){
-  var actual = logic.deleteTodo([{a: 1, id: 1}], 1);
+test("filter to delete", function(t) {
+  var actual = logic.deleteTodo([{ a: 1, id: 1 }], 1);
   var expected = [];
-  t.deepEquals(actual, expected, ' should delete object to array');
+  t.deepEquals(actual, expected, " should delete object to array");
   t.end();
 });
 
-test('map to mark', function(t){
-  var actual = logic.markTodo([{a: 1, id: 1}], 1);
-  var expected = [{a: 1, id: 1, done: true}];
-  t.deepEquals(actual, expected, ' should mark object to array');
+test("map to mark", function(t) {
+  var actual = logic.markTodo([{ a: 1, id: 1 }], 1);
+  var expected = [{ a: 1, id: 1, done: true }];
+  t.deepEquals(actual, expected, " should mark object to array");
   t.end();
 });
